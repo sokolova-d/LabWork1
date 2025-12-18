455c2f570d56aae3c623b8479b8a777fc1b1c227
Sokolova Dasha <st141899@student.spbu.ru>
2025-12-19 00:12:12 +0300

    Add files for image headers

---
+++ BMPHeader.h
@@ -0,0 +1,54 @@
+/*
+* Sokolova Dasha
+* st141899@student.spbu.ru
+* My project number five.
+*/
+
+#ifndef BMP_HEADER_H
+#define BMP_HEADER_H
+
+#include <cstdint>
+#include <fstream>
+
+#pragma pack(push, 1)
+//first header of the file
+struct BMPFileHeader {
+    uint16_t bfType;
+    uint32_t bfSize;
+    uint16_t bfReserved1;
+    uint16_t bfReserved2;
+    uint32_t bfOffBits;
+};
+//main header
+struct BMPInfoHeader {
+    uint32_t biSize;
+    int32_t  biWidth;
+    int32_t  biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    int32_t  biXPelsPerMeter;
+    int32_t  biYPelsPerMeter
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+};
+
+#pragma pack(pop)
+
+class BMPHeader {
+public:
+//constuctor
+BMPHeader();
+BMPHeader(int width, int height);
+
+void read(std::ifstream& in);
+void write(std::ofstream& out) const;
+
+int rowPadding() const;
+
+BMPFileHeader file;
+BMPInfoHeader info;
+};
+
+#endif
